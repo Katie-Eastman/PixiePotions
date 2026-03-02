402c7c39ed9b02c09237a94e6a94d2ab64f96b6f
Katie Eastman <katherineeastman@my.cwi.edu>
2026-03-01 21:44:30 -0700

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,4 +1,26 @@
-//when the user submits the contact form, they will be thanked for their successful submission
+window.addEventListener("DOMContentLoaded", loadedHandler);
+
+function loadedHandler() {
+	const themeButton = document.getElementById("themeToggleButton");
+	themeButton.addEventListener("click", toggleTheme);
+	
+	const savedTheme = localStorage.getItem("theme");
+	if (savedTheme == "light") {
+		document.body.classList.add("lighttheme");
+	}
+}
+
+function toggleTheme() {
+	document.body.classList.toggle("lighttheme");
+	
+	if (document.body.classList.contains("lighttheme")) {
+		localStorage.setItem("theme", "light");
+	}
+	else {
+		localStorage.setItem("theme", "dark");
+	}
+}
+
 function confirmSubmit(){
 	alert("Thank you for contacting us! A member of our team will get back to you as soon as possible.");
 }
